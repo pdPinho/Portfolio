b6df854f4f051a2cf311cdcecff969056e0b6d66
pdPinho <pd.pinho@ua.pt>
2025-03-17 16:02:15 +0000

    upload bunch of assets; add final pages; adjust texts.

---
+++ src/scripts/autoplay.js
@@ -0,0 +1,18 @@
+export function handleAutoplay() {
+    const videos = document.querySelectorAll('.contentImage');
+    videos.forEach(video => {
+    if (window.innerWidth < 640) {
+        video.setAttribute('autoplay', '');
+        video.setAttribute('loop', '');
+    } 
+    else {
+        video.removeAttribute('autoplay');
+        video.removeAttribute('loop');
+        video.pause();
+        video.currentTime = 0;
+    }
+});
+}
+
+window.addEventListener('load', handleAutoplay);
+window.addEventListener('resize', handleAutoplay);
